ef2812f9bce610fb33d13a19c06fd3895fca23be
Jani Suoranta <janisuoranta@icloud.com>
2021-04-11 13:40:37 +0300

    [MOD] tsx basics header, constants

---
+++ peka-app/src/utils/constants.ts
@@ -0,0 +1,38 @@
+// APP
+// APP TEXT
+export const APP_TITLE = 'Peka - Pelikaveri';
+
+// HEADER
+// HEADER TITLE
+export const HEADER_TITLE_LAJI = 'Laji';
+export const HEADER_TITLE_AIKA = 'Aika';
+export const HEADER_TITLE_PAIKKA = 'Paikka';
+export const HEADER_TITLE_ILMOITUSTYYPPI = 'Ilmoitustyyppi';
+
+// NAV
+// NAV TITLES
+export const NAV_ILMOITUKSET = 'Ilmoitukset';
+// NAV JÄTÄ ILMOITUS
+export const NAV_JATA_ILMOITUS = 'Jätä ilmoitus';
+export const NAV_JATA_ILMOITUS_OTSIKKO = 'Ilmoituksen otsikko';
+export const NAV_JATA_ILMOITUS_LAJI = 'Laji';
+export const NAV_JATA_ILMOITUS_KUVAUS = 'Kuvaus';
+export const NAV_JATA_ILMOITUS_AIKA = 'Aika';
+export const NAV_JATA_ILMOITUS_PAIKKA = 'Paikka';
+export const NAV_JATA_ILMOITUS_NAYTA_PUH_NRO = 'Näytä puhelinnumerosi';
+// NAV PROFIILI
+export const NAV_PROFIILI = 'Profiili';
+export const NAV_PROFIILI_LISÄÄ_PROFIILIKUVA = 'Lisää profiilikuva';
+export const NAV_PROFIILI_NIMI = 'Nimi';
+export const NAV_PROFIILI_PUH_NRO = 'Puhelinnumero';
+export const NAV_PROFIILI_BIO = 'Vapaamuotoinen kuvaus itsestäsi';
+export const NAV_PROFIILI_OMAT_ILMOITUKSET = 'Omat ilmoitukset';
+export const NAV_PROFIILI_HAKUVAHDIT = 'Hakuvahdit';
+// NAV VIESTIT
+export const NAV_VIESTIT = 'Viestit';
+// NAV MUUT
+export const NAV_MUUT = 'Muut';
+export const NAV_MUUT_ASETUKSET = 'Asetukset';
+export const NAV_MUUT_MUUTA_YKSITYISYYSASETUKSIA = 'Muuta yksityisyysasetuksia';
+export const NAV_MUUT_EVASTEET = 'Evästeet';
+export const NAV_MUUT_SIJAINTIASETUKSET = 'Sijaintiasetukset';
